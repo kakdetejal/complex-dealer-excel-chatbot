3162094e7438471d10b031bf1cca4b8337a440b2
kakdetejal <kakdetejal98@gmail.com>
2026-05-04 15:29:13 +0530

    Delete embeddings.py

--- embeddings.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("all-mpnet-base-v2")
-
-def get_embeddings(texts):
-    return model.encode(texts)
-
-def embed_query(query):
-    return model.encode(query)
